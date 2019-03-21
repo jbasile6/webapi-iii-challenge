62d4ddde5a20f973fdb36c2d37137b64c7870b0b
James Basile <jbasile145@gmail.com>
2019-03-21 14:38:48 -0400

    created .env file with dynamic port and greeting

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// code away!
+require('dotenv').config();
 
 
 const server = require('./server');
 
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
 
-server.listen(4000, () => {
-    console.log('\n* Server Running on http://localhost:4000 *\n');
+server.listen(port, () => {
+    console.log(`\n*${greeting}: Server Running on http://localhost:${port} *\n`);
 });
